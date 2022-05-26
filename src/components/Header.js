33852a61727fcd1226ea38c386db0e99f7a3060f
Lý Thanh Bách <40271986+edwardly1002@users.noreply.github.com>
2022-05-26 20:10:18 +0700

    First push

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Box from '@mui/material/Box';
+
+function Header() {
+  return (
+    <Box>
+        <Box>
+            <img src='hcmut.png'/>
+        </Box>
+    </Box>
+  )
+}
+
+export default Header
